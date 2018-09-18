e16d9468eb76547ef9543cb83c1a09613fc37705
rokimoto <rachel.okimoto@gmail.com>
2018-09-18 14:34:39 -0700

    adds config for mobile and small desktop.  adds styling for nav for m… (#4)
    * adds config for mobile and small desktop.  adds styling for nav for mobile and small desktop.  adds mobile hamburger animation.  removes traces of jquery
    
    * adds missing semi colons

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-$(document).ready(function() {
+document.addEventListener('DOMContentLoaded', function(event) {
+  var navHamburger = document.getElementById('nav-hamburger');
+  var navItems = document.getElementById('nav-items');
+
+  navHamburger.addEventListener('click', function(e) {
+    navHamburger.classList.toggle('active');
+    navItems.classList.toggle('active');
+  });
 
 });
+
